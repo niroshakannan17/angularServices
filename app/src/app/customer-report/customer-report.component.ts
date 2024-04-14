d1e05f208e6073d0ae9e3336d7bb43a329108f93
nirosha <niroshakannan17@gmail.com>
2024-04-14 19:16:33 +0530

    sevice can use more componets also

--- app/src/app/customer-report/customer-report.component.ts
+++ app/src/app/customer-report/customer-report.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { CustomerService } from '../service/customer.service';
+import { Customer } from '../models/customer';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-customer-report',
   templateUrl: './customer-report.component.html',
   styleUrl: './customer-report.component.css'
 })
-export class CustomerReportComponent {
+export class CustomerReportComponent implements OnInit{
+
+  constructor( private custService:CustomerService){}
+
+  customers:Customer[]=[];
+
+  ngOnInit(): void {
+    this.customers = this.custService.get();
+  }
+
+
   
 }
